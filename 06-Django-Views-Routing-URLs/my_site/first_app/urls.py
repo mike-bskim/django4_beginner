1239f044b27e56647aeed371198d81636d2cb0f2
mike.bskim <mike.bskim@gmail.com>
2022-10-18 14:39:28 +0200

    02_view_urls

--- 06-Django-Views-Routing-URLs/my_site/first_app/urls.py
+++ 06-Django-Views-Routing-URLs/my_site/first_app/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.simple_view),
-    path('<int:num1>/<int:num2>',views.add_view),
-    path('<int:page_number>',views.page_num_view),
-    path('<str:topic>/',views.news_view,name='topic-page'),
+    # 실제 url은 프로젝트 urls.py 에서 설정하므로 여기선 생략,
+    # 생략한 부분을 추가하면, 프로젝트 path('first_app/') + 앱url path('simple_view') 로 구성된다.
+    # domain.com/first_app/simple_view 로 구성됨,
+    # view 인자는 실제 연결할 views의 함수 이름을 추가함,
+    path('simple_view', views.simple_view,)
 ]
