65f322a37f7a83f9a1c28545f74d1f6abfd7e1f5
mike.bskim <mike.bskim@gmail.com>
2022-10-19 13:47:44 +0200

    05_404_exception

--- 06-Django-Views-Routing-URLs/my_site/first_app/views.py
+++ 06-Django-Views-Routing-URLs/my_site/first_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, Http404, HttpResponseNotFound
 
 # Create your views here.
 def simple_view(request):
@@ -18,7 +18,13 @@ def finance_view(request):
     return HttpResponse(articles['finance'])
 
 def news_view(request, topic):
-    return HttpResponse(articles[topic])
+    try:
+        result = articles[topic]
+        return HttpResponse(result)
+    except:
+        # result = 'No page for that topic~'
+        # return HttpResponseNotFound(result)
+        raise Http404('404 generic ERROR') # 404.html 만들어서 처리 예정임,
 
 def add_view(request, num1, num2):
     # domain.com/first_app/num1/num2 --> num1+num2
