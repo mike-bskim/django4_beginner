5fe6f2edc8537bed21e72ef19f317282929b422b
mike.bskim <mike.bskim@gmail.com>
2022-10-26 00:21:43 +0200

    11_variable

--- 07-Django-and-Templates/my_site/my_app/urls.py
+++ 07-Django-and-Templates/my_site/my_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.example_view) # domain.com/my_app
+    path('', views.example_view), # domain.com/my_app
+    path('variable/', views.variable_view), # domain.com/my_app
 ]
--- 07-Django-and-Templates/my_site/my_app/views.py
+++ 07-Django-and-Templates/my_site/my_app/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 def example_view(request):
     # my_app/templates/my_app/example.html
     # render 내부의 폴더는 상기 주석의 후반부 의미함. 전반부는 프로젝트의 settings.py 에서 설정함
-    return render(request, 'my_app/example.html')    
+    return render(request, 'my_app/example.html')
+
+def variable_view(request):
+    my_var = {'first_name':'Rosaland', 'last_name':'Franklin', 
+        'some_list':[1,2,3], 'some_dict':{'inside_key':'inside_value'}
+    }
+    return render(request, 'my_app/variable.html', context=my_var)
