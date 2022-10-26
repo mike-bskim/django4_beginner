92bced2d9887ad0ce1c70b4e5cdb9ef0c845530c
mike.bskim <mike.bskim@gmail.com>
2022-10-26 22:02:59 +0200

    14_url

--- 07-Django-and-Templates/my_site/my_app/urls.py
+++ 07-Django-and-Templates/my_site/my_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+# register the app namespace
+# RUL NAMES
+app_name = 'my_app'
+
 urlpatterns = [
-    path('', views.example_view), # domain.com/my_app
-    path('variable/', views.variable_view), # domain.com/my_app
+    path('', views.example_view, name= 'example'), # domain.com/my_app
+    path('variable/', views.variable_view, name='variable'), # domain.com/my_app
 ]
