044ff9c8768359e1c7531849cdb2e4a6a0ffe51d
mike.bskim <mike.bskim@gmail.com>
2022-11-01 00:33:52 +0100

    18_update_model

--- 08-Django-Models-Databases/my_site/office/models.py
+++ 08-Django-Models-Databases/my_site/office/models.py
@@ -6,7 +6,8 @@ class Patient(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30) 
     age = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(120)]) 
-    # heartrate = models.IntegerField(default=60,validators=[MinValueValidator(1),MaxValueValidator(300)])
+    heartrate = models.IntegerField(default=60,validators=[MinValueValidator(1),MaxValueValidator(300)])
+    # heartrate = models.IntegerField(default=60)
 
     def __str__(self):
         return f"{self.last_name}, {self.first_name} is {self.age} years old."
