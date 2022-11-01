0df4d6cfefaedb6ec30f4e3f8b0f716824f91adb
mike.bskim <mike.bskim@gmail.com>
2022-11-01 16:15:56 +0100

    19_connect_template_models

--- 08-Django-Models-Databases/my_site/office/views.py
+++ 08-Django-Models-Databases/my_site/office/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from . import models # 모델 추가
 
 # Create your views here.
+def list_patients(request):
+    all_patients = models.Patient.objects.all()
+    context_list = {'patients' : all_patients}
+
+    return render(request, 'office/list.html', context=context_list)
