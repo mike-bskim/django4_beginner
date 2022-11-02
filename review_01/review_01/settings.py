b3548dfd01491067bac9b0486ab61ded6878e1de
mike.bskim <mike.bskim@gmail.com>
2022-11-02 14:00:54 +0100

    20-03-1_app_setting

--- review_01/review_01/settings.py
+++ review_01/review_01/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'first_app.apps.FirstAppConfig', # <=== 앱인식을 위한 추가
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
