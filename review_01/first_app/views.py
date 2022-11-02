d5b7aa2da996ba25c061d6992ca1e04330ab6db6
mike.bskim <mike.bskim@gmail.com>
2022-11-02 15:46:44 +0100

    20-07_connect_template_models

--- review_01/first_app/views.py
+++ review_01/first_app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from . import models
 # from django.http.response import HttpResponse
 
 # Create your views here.
 def list_patients(request):
+    all_patients = models.Patient.objects.all()
+    context_list = {'patients' : all_patients}
+
     # return HttpResponse('patients list')
-    return render(request, 'first_app/list.html')
+    return render(request, 'first_app/list.html', context=context_list)
