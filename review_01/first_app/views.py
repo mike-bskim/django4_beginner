9112f41694be1326f8d0f0ac97af3b474009b6b0
mike.bskim <mike.bskim@gmail.com>
2022-11-02 14:49:06 +0100

    20-04_app_template

--- review_01/first_app/views.py
+++ review_01/first_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+# from django.http.response import HttpResponse
 
 # Create your views here.
+def list_patients(request):
+    # return HttpResponse('patients list')
+    return render(request, 'first_app/list.html')
