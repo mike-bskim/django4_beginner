cac36c26282ce2a6bd48f05fc7854ca7f4c50fee
mike.bskim <mike.bskim@gmail.com>
2022-11-03 14:38:24 +0100

    21_admin

--- review_01/first_app/admin.py
+++ review_01/first_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from first_app.models import Patient
 
 # Register your models here.
+# admin.site.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    # fields = ['heartrate', 'last_name','first_name', 'age']
+    fieldsets = [
+        ('USER NAME', {'fields':['first_name','last_name']}),
+        ('HEALTH CONDITIONS', {'fields':['age','heartrate']}),
+
+    ]
+
+admin.site.register(Patient, PatientAdmin)
--- review_01/first_app/models.py
+++ review_01/first_app/models.py
@@ -10,4 +10,4 @@ class Patient(models.Model):
     heartrate = models.IntegerField(default=60,validators=[MinValueValidator(1),MaxValueValidator(300)])
 
     def __str__(self):
-        return f"{self.last_name}, {self.first_name} is {self.age} years old."
+        return f"{self.last_name}, {self.first_name} is {self.age} years old({self.heartrate})."
