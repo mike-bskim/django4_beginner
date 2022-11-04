44ecc8dd424ef5320152195e00eea69b85c57e74
mike.bskim <mike.bskim@gmail.com>
2022-11-04 17:02:46 +0100

    23_forms_01_basic_form

---
+++ 10-Django-Forms/my_site/cars/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class ReviewForm(forms.Form):
+    first_name = forms.CharField(label = 'First Name', max_length=100)
+    last_name = forms.CharField(label = 'Last Name', max_length=100)
+    email = forms.CharField(label = 'Email', max_length=100)
+    review = forms.CharField(label='Please write your review here')
+
+    
---
+++ 10-Django-Forms/my_site/cars/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from . import forms
+
+# Create your views here.
+def rental_review(request):
+    # POST REQEUEST --> Form Contents --> Thank you
+    if request.method == 'POST':
+        form = forms.ReviewForm(request.POST)
+        if form.is_valid():
+            # {'first_name':'Jose', 'last_name' , 'email' ~~~~}
+            print(form.cleaned_data)
+            return redirect(reverse('cars:thank_you'))
+    else:
+        form = forms.ReviewForm()
+    # return render(request, 'cars/rental_review.html')
+    return render(request, 'cars/rental_review.html', context={'form':form})
+
+def thank_you(request):
+    return render(request, 'cars/thank_you.html')    
