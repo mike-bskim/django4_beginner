23d35b2999d198126c890f384ff3604e85387e68
mike.bskim <mike.bskim@gmail.com>
2022-12-14 18:30:20 +0100

    23_forms_03_widget_style

--- 10-Django-Forms/my_site/cars/forms.py
+++ 10-Django-Forms/my_site/cars/forms.py
@@ -4,6 +4,7 @@ class ReviewForm(forms.Form):
     first_name = forms.CharField(label = 'First Name', max_length=100)
     last_name = forms.CharField(label = 'Last Name', max_length=100)
     email = forms.CharField(label = 'Email', max_length=100, required=False)
-    review = forms.CharField(label='Please write your review here', required=False)
+    review = forms.CharField(label='Please write your review here', required=False, 
+                             widget=forms.Textarea(attrs={'class':'myform', 'rows':'2', 'cols':'2'}))
 
     
--- 10-Django-Forms/my_site/my_site/settings.py
+++ 10-Django-Forms/my_site/my_site/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        'APP_DIRS': True,
+        'APP_DIRS': True, # <=== 기본설정 확인
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
