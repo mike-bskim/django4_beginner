50b46491bc7e247d2b63f3aa5b532672cf624de2
mike.bskim <mike.bskim@gmail.com>
2022-12-15 21:00:01 +0100

    24_CBV01_TemplateView03_done

--- 11-Django-Class-Based-Views/school/classroom/urls.py
+++ 11-Django-Class-Based-Views/school/classroom/urls.py
@@ -5,5 +5,7 @@ app_name = 'classroom'
 
 # domain.com/classroom
 urlpatterns = [
-    path('', views.home_view, name='home'), # path expects a function!
+    # path('', views.home_view, name='home'), # path expects a function!
+    path('', views.HomeView.as_view(), name='home'), # 클래스를 가지고 경로에 대한 함수를 반환
+    path('thank_you/', views.ThankYouView.as_view(), name='thank_you'), 
 ]
