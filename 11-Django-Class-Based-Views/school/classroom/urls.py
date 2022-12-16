d8a0d4ee4b20b9882e66916c99634a028bbb15f0
mike.bskim <mike.bskim@gmail.com>
2022-12-16 18:07:48 +0100

    24_CBV02_FormView

--- 11-Django-Class-Based-Views/school/classroom/urls.py
+++ 11-Django-Class-Based-Views/school/classroom/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # path('', views.home_view, name='home'), # path expects a function!
     path('', views.HomeView.as_view(), name='home'), # 클래스를 가지고 경로에 대한 함수를 반환
     path('thank_you/', views.ThankYouView.as_view(), name='thank_you'), 
+    path('contact/', views.ContactFormView.as_view(), name='contact'), 
 ]
--- 11-Django-Class-Based-Views/school/classroom/views.py
+++ 11-Django-Class-Based-Views/school/classroom/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+from classroom.forms import ContactForm
 
 # Create your views here.
 # def home_view(request):
@@ -11,3 +12,20 @@ class HomeView(TemplateView):
 class ThankYouView(TemplateView):
     template_name = 'classroom/thank_you.html'
     
+class ContactFormView(FormView): # <=== 추가 부분
+    # 1. 폼 클래스 연결, 
+    form_class = ContactForm # 인스턴스 생성하기전에 연결만 함.
+    # 2. 폼과 연결할 template_name 설정
+    template_name = 'classroom/contact.html' 
+
+    # 3. 정상일때 이동할 URL 설정.
+    # success URL?, URL is not a template.html
+    success_url = '/classroom/thank_you/' 
+    # 여기서 reverse 를 사용하고 싶다면 reverse_lazy() 를 사용해야 한다(lazy는 object 리턴). 외울것.
+    # success_url = reverse_lazy('classroom:thank_you')
+
+    # what to do with form?
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        # ContactForm(reques.POST)
+        return super().form_valid(form)
